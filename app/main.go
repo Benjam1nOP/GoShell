c2bec626cecad75feeffaa948c26d93103d81a1f
Benjam1nOP <lakshaydheerjain@gmail.com>
2025-11-03 23:41:43 +0530

    added for loop to loop infinitely

--- app/main.go
+++ app/main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	
-	fmt.Fprint(os.Stdout, "$ ")
-	command, err:= bufio.NewReader(os.Stdin).ReadString('\n')
-	if err!=nil{
-		fmt.Printf("-Error Reading the input")
-		os.Exit(1)
+
+	reader := bufio.NewReader(os.Stdin)
+	for{
+		fmt.Fprint(os.Stdout, "$ ")
+
+		command, err:= reader.ReadString('\n')
+
+		if err!=nil{
+			if err == io.EOF{
+				break
+			}
+			fmt.Printf("-Error Reading the input")
+			os.Exit(1)
+		}
+
+		command = strings.TrimRight(command, "\r\n")
+		
+		fmt.Printf("%s: command not found\n",command)
 	}
-	command = strings.TrimRight(command, "\r\n")
-	fmt.Printf("%s: command not found",command)
-	
 }
