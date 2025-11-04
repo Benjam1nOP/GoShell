1d7622b263685c0bee825664ac42afc98efa7b04
Benjam1nOP <lakshaydheerjain@gmail.com>
2025-11-04 17:58:17 +0530

    added the switch, array for type checking, os output, and error for single command

--- app/main.go
+++ app/main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"slices"
 	"strings"
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
+	existingfunc := []string {"echo","exit","type"} // array for the existing commands
+
 	for{
 		fmt.Fprint(os.Stdout, "$ ")
 
@@ -28,21 +31,56 @@ func main() {
 		//command = strings.TrimRight(command, "\r\n")
 		command = strings.TrimSpace(command)  // In linux env \n is sent after text in windows \r\n is sent
 		cmd :=strings.ToLower(command) // to lower ECho to echo
-		switch cmd{
-			case "exit 1":
+		switch {
+			case cmd == "exit 1":
 				os.Exit(1)
-			case "exit 0":
+			case cmd == "exit 0":
 				os.Exit(0)
+
+			case strings.HasPrefix(cmd, "type"):{
+				arg := strings.TrimSpace(cmd[4:])
+				if arg == ""{	// TrimSpace will Trim "type echo" to "echo" and for "type " (type and space) from " " to ""
+					fmt.Fprintln(os.Stderr, "Invalid no. of arguments for Command Type")
+					break
+				}
+				if slices.Contains(existingfunc, arg){ // checking if it is an existing command or not in the array
+					fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", arg)
+				}else {
+					fmt.Fprintf(os.Stderr, "%s: not found\n", arg)
+				}
+			}
+
+			case strings.HasPrefix(cmd, "echo"):{
+				arg := strings.TrimSpace(cmd[4:])
+				//fmt.Println(cmd[4:])
+				if arg == "" {
+					fmt.Fprintln(os.Stderr, "Invalid no. of parameter for echo")
+					break
+				}
+				fmt.Fprintln(os.Stdout, cmd[5:])
+			}
+
+			default: 
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", command)
+		}
+		/*if strings.HasPrefix(cmd, "type"){
+			arg := strings.TrimSpace(cmd[5:])
+			if arg == "exit" || arg == "echo"{
+				fmt.Printf("%s is a shell builtin", arg)
+			}else{
+			fmt.Printf("%s: not found\n", cmd)
+			}
+			return
 		}
 
-		if strings.HasPrefix(cmd, "echo"){
+		if strings.HasPrefix(cmd, "echo"){ // checking prefix
 			if len(cmd)==4{
 				fmt.Println("Invalid no. of parameter for echo")
 				os.Exit(1)
 			}
 			fmt.Println(cmd[5:])
-		}else {
-			fmt.Printf("%s: command not found\n", command)
+			return
 		}
+		fmt.Printf("%s: command not found\n", command) */
 	}
 }
