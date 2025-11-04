c9ac44f50d800e886265c0aadfd24bbea55624f5
Benjam1nOP <lakshaydheerjain@gmail.com>
2025-11-04 15:09:18 +0530

    added the exit 1 and exit 0 as hardcode

--- app/main.go
+++ app/main.go
@@ -26,7 +26,12 @@ func main() {
 		//fmt.Printf("%d",len(command))	check what is getting stored after input and it is text with \r\n
 
 		//command = strings.TrimRight(command, "\r\n")
-
+		if command == "exit 1\r\n" {
+			os.Exit(1)
+		}
+		if command == "exit 0\r\n" {
+			os.Exit(0)
+		}
 		fmt.Printf("%s: command not found\n", strings.TrimSpace(command))
 	}
 }
